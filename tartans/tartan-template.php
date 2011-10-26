79783ff01a57a60e4de33ea9c86a7c385a5a9764
Lyza Danger Gardner <lyza@lyza.com>
2011-10-25 19:59:48 -0700

    First stab at a basic HTML template for tartans (to be written to)

---
+++ tartans/tartan-template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+<html> 
+	<head> 
+	<title><?php print $this->name ?>: The Tartanator</title> 
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <link rel="stylesheet" href="../css/jquery.mobile-1.0rc1.min.css" />
+  <script src="../js/jquery-1.6.4.min.js"></script>
+  <script src="../js/jquery.mobile-1.0rc1.min.js"></script>
+</head> 
+<body> 
+
+<div data-role="page" id="<?php print $this->getBaseName(); ?>">
+  <style type="text/css">
+    #<?php print $this->getBaseName(); ?> { background-image: url('images/<?php print $this->getBaseName(); ?>-240.png'); }
+  </style>
+	<div data-role="header" data-position="fixed">
+    <a href="../tartans.html" data-rel="back" data-direction="reverse" data-icon="back" />Back</a>
+		<h1><?php print $this->name; ?></h1>
+	</div><!-- /header -->
+
+	<div data-role="content">
+    
+	</div><!-- /content -->
+	<div data-role="footer" data-position="fixed" data-theme="c">
+		Bring forrit the tartan!
+	</div><!-- /footer -->
+</div><!-- /page -->
+
+</body>
+</html>
