cd89c03220c6ae0a1329c3cf3da4ad8f68b70a2f
Lyza Danger Gardner <lyza@lyza.com>
2011-10-28 10:56:55 -0700

    OK. I think I might finally have this situation fairly config-less

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 if (file_exists('config.inc')) {
   require_once('config.inc');
 } else {
+  define('TARTAN_DOC_ROOT', '/' .  substr(dirname(__FILE__), strlen($_SERVER['DOCUMENT_ROOT'])));
   define('PUBLIC_TARTAN_DIR', '/tartans/');
   define('TARTAN_XML_DIR', dirname($_SERVER['SCRIPT_FILENAME']) . '/tartans/data/');
   define('TARTAN_IMAGE_DIR', dirname($_SERVER['SCRIPT_FILENAME']) . '/tartans/images/');
