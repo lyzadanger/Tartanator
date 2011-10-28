ebec0e362f6544471b502274d5c3b67cd170ad4d
Lyza Danger Gardner <lyza@lyza.com>
2011-10-28 13:31:05 -0700

    One day this config crap will be behind me.

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ if (file_exists('config.inc')) {
   define('TARTAN_DOC_ROOT', '/' .  substr(dirname(__FILE__), strlen($_SERVER['DOCUMENT_ROOT'])));
   define('PUBLIC_TARTAN_DIR', TARTAN_DOC_ROOT . '/tartans/');
   // Absolute filesystem paths
-  define('TARTAN_XML_DIR', dirname($_SERVER['SCRIPT_FILENAME']) . '/tartans/data/');
-  define('TARTAN_IMAGE_DIR', dirname($_SERVER['SCRIPT_FILENAME']) . '/tartans/images/');
-  define('TARTAN_HTML_DIR', dirname($_SERVER['SCRIPT_FILENAME']) . '/tartans/');
+  define('TARTAN_XML_DIR', dirname(__FILE__) . '/tartans/data/');
+  define('TARTAN_IMAGE_DIR', dirname(__FILE__) . '/tartans/images/');
+  define('TARTAN_HTML_DIR', dirname(__FILE__) . '/tartans/');
 }
